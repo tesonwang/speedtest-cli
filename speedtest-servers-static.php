aa0e181a1db46513ad55ca4bff1a8f296ab84471
teson <teson.wang@cortina-access.com>
2022-12-06 10:05:54 +0800

    Add php files
    Add speedtest-config.php / speedtest-servers-static.php

---
+++ speedtest-servers-static.php
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<settings>
+<servers><server url="http://qw-tcspt2.wifly.com.tw:8080/speedtest/upload.php" lat="24.1632" lon="120.6747" name="Taichung" country="Taiwan" cc="TW" sponsor="FarEasTone Telecom" id="50113"  host="qw-tcspt2.wifly.com.tw:8080" />
+<server url="http://seednet-tc1.seed.net.tw:8080/speedtest/upload.php" lat="24.1500" lon="120.6667" name="Taichung" country="Taiwan" cc="TW" sponsor="SEEDNET" id="40727"  host="seednet-tc1.seed.net.tw:8080" />
+<server url="http://tcspt2.friday.tw:8080/speedtest/upload.php" lat="24.1477" lon="120.6736" name="Taichung City" country="Taiwan" cc="TW" sponsor="Friday Media" id="19210"  host="tcspt2.friday.tw:8080" />
+<server url="https://ncc-speed.kbro.com.tw:8080/speedtest/upload.php" lat="24.0518" lon="120.5161" name="彰化" country="Taiwan" cc="TW" sponsor="新頻道" id="14656"  host="ncc-speed.kbro.com.tw:8080" />
+<server url="http://hc1.chtm.hinet.net:8080/speedtest/upload.php" lat="24.8167" lon="120.9833" name="Hsinchu" country="Taiwan" cc="TW" sponsor="Chunghwa Mobile" id="18450"  host="hc1.chtm.hinet.net:8080" />
+<server url="http://srv8.speedtest.aptg.com.tw:8080/speedtest/upload.php" lat="24.8167" lon="120.9833" name="Hsinchu" country="Taiwan" cc="TW" sponsor="Asia Pacific Telecom" id="29109"  host="srv8.speedtest.aptg.com.tw:8080" />
+<server url="http://fetyl1.seed.net.tw:8080/speedtest/upload.php" lat="23.9596" lon="120.5855" name="Yuanlin City" country="Taiwan" cc="TW" sponsor="FarEasTone Telecom" id="17204"  host="fetyl1.seed.net.tw:8080"
+/>
+<server url="http://srv9.speedtest.aptg.com.tw:8080/speedtest/upload.php" lat="23.9333" lon="120.5333" name="Changhua" country="Taiwan" cc="TW" sponsor="Asia Pacific Telecom" id="29110"  host="srv9.speedtest.aptg.com.tw:8080" />
+<server url="http://ch1.chtm.hinet.net:8080/speedtest/upload.php" lat="23.9333" lon="120.5333" name="Changhua" country="Taiwan" cc="TW" sponsor="Chunghwa Mobile" id="18611"  host="ch1.chtm.hinet.net:8080" />
+<server url="http://ntv3-1.speedtest.idv.tw:8080/speedtest/upload.php" lat="23.9610" lon="120.9719" name="Nantou" country="Taiwan" cc="TW" sponsor="Chief Telecom" id="18477"  host="ntv3-1.speedtest.idv.tw:8080" />
+</servers>
+</settings>
